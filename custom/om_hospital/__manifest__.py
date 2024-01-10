d334d2efed5155d26acba33c11e08d0808911285
Shaharyar Ahmed <shah.s.a.ahmed@gmail.com>
2024-01-10 14:12:06 +0500

    Add Doctor Field + Sample Data in Tree and Kanban View (39)

--- custom/om_hospital/__manifest__.py
+++ custom/om_hospital/__manifest__.py
@@ -21,6 +21,7 @@
         'views/kids_view.xml',
         'views/patient_gender_view.xml',
         'views/appointment_view.xml',
+        'views/doctor_view.xml',
         'views/sale.xml'
     ],
     'installable': True,
---
+++ custom/om_hospital/models/doctor.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+class HospitalDoctor(models.Model):
+    _name = "hospital.doctor"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "hospital doctor"
+
+    doctor_name = fields.Char(string="Name", required=True, tracking=True)
+    age = fields.Integer(string="Age", required=True, tracking=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ], required=True, default='male', tracking=True)
+    note = fields.Text(string="Description")
+    image = fields.Binary(steing="Doctor Image")
