db113e26d8c8e6c30d6ad4bb7a7a88f3dd40254e
Shaharyar Ahmed <shah.s.a.ahmed@gmail.com>
2024-01-24 18:11:09 +0500

    Task 1 (inherit fields 2)

--- custom/om_hospital/models/test.py
+++ custom/om_hospital/models/test.py
@@ -56,36 +56,36 @@ class HospitalTest(models.Model):
 
     test_name = fields.Char(string="Name")
 
-    lahore = fields.Boolean(string="Lahore")
-    karachi = fields.Boolean(string="Karachi")
-    islamabad = fields.Boolean(string="Islamabad")
-    # lahore = fields.Boolean(string="Lahore", invisible=False)
-    # karachi = fields.Boolean(string="Karachi", invisible=False)
-    # islamabad = fields.Boolean(string="Islamabad", invisible=False)
+    lahore = fields.Boolean(string="Lahore", invisible=True)
+    karachi = fields.Boolean(string="Karachi", invisible=True)
+    islamabad = fields.Boolean(string="Islamabad", invisible=True)
 
     def toggle_lahore(self):
+        self.karachi = False
+        self.islamabad = False
         self.lahore = True
 
     def toggle_karachi(self):
+        self.islamabad = False
+        self.lahore = False
         self.karachi = True
 
     def toggle_islamabad(self):
+        self.lahore = False
+        self.karachi = False
         self.islamabad = True
 
-    #
     # def toggle_lahore(self):
-    #     # if self.lahore:
-    #     self.lahore = True
-    #     # else:
-    #     #     self.is_lahore_visible = False
+    #     if self.lahore == False:
+    #         self.lahore = not self.lahore
     #
     # def toggle_karachi(self):
-    #     # if self.karachi:
-    #     self.karachi = True
+    #     if self.karachi == False:
+    #         self.karachi = not self.karachi
     #
     # def toggle_islamabad(self):
-    #     # if self.islamabad:
-    #     self.islamabad = True
+    #     if self.islamabad == False:
+    #         self.islamabad = not self.islamabad
 
     def reset_all(self):
         self.lahore = False
